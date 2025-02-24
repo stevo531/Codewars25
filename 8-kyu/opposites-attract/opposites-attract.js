02a190ccf4220985a5b04b3537338749ef4e246e
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-02-24 15:30:12 +0200

    Update solution - CodeHub

--- 8-kyu/opposites-attract/opposites-attract.js
+++ 8-kyu/opposites-attract/opposites-attract.js
@@ -1,5 +1,5 @@
 function lovefunc(flower1, flower2){
- if(flower1 % 2 === 0 && flower2 % 2 === 0 ){
+ if(flower1 % 2 === 0 && flower2 % 2 !== 0 ){
    return true
  } else if(flower1 % 2 !== 0 && flower2 % 2 === 0){
    return true;
