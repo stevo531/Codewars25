45e9891642fe2c6bb1216cfe8362e3b582f223a3
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-02-27 11:36:07 +0200

    Add solution - CodeHub

---
+++ 7-kyu/sentences-with-functions/sentences-with-functions.js
@@ -0,0 +1,30 @@
+function Adam() {
+  return 'Adam'
+}
+function has() {
+  return 'has'
+}
+function a() {
+  return 'a'
+}
+function dog() {
+  return 'dog'
+}
+function The() {
+  return 'The'
+}
+function name() {
+  return 'name'
+}
+function of() {
+  return 'of'
+}
+function the() {
+  return 'the'
+}
+function is() {
+  return 'is'
+}
+function also() {
+  return 'also'
+}
