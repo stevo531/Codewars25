3b80a16eee429beee26c0c19257ea0433f4a6c9f
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-03-04 14:41:46 +0200

    Update solution - CodeHub

--- 6-kyu/split-strings/split-strings.js
+++ 6-kyu/split-strings/split-strings.js
@@ -1,8 +1,7 @@
- 
 function solution(str){
  let arr = [];
   for(let i = 0; i < str.length; i++){
-    let pairChar = str[i] + (str[i] + 1)
+    let pairChar = str[i] + str[i + 1] || '-' 
     arr.push(pairChar)
   }
   return arr
