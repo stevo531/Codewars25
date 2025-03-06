f3fb44a87dd13da68225620a72a2869037c1e6c6
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-03-06 10:43:04 +0200

    Add solution - CodeHub

---
+++ 6-kyu/convert-string-to-camel-case/convert-string-to-camel-case.js
@@ -0,0 +1,10 @@
+ 
+function toCamelCase(str) {
+   const words = str.split(/[-_]/);
+     for (let i = 1; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+ return words.join('');
+}
+​
+​
