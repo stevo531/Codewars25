abc5957d2728493e7459a2e3a934817e3e26fba6
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-03-07 10:00:36 +0200

    Update solution - CodeHub

--- 5-kyu/human-readable-time/human-readable-time.js
+++ 5-kyu/human-readable-time/human-readable-time.js
@@ -1,6 +1,7 @@
+ 
 function humanReadable (seconds) {
   let hours = Math.floor(seconds/3600)
-  let minutes = Math.floor(seconds / 31536000) % 86400) % 3600) /60)
+  let minutes = Math.floor((((seconds / 31536000) % 86400) % 3600) /60)
   let secs = (seconds / 31536000) % 86400) % 3600) % 60)
   if(hours < 10){
     hours = '0'+ hours;
