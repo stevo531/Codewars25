5e446979f8e1dcef30d579af038719026c068a5e
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-04-03 10:19:07 +0200

    Update solution - CodeHub

--- 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.js
+++ 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.js
@@ -1,17 +1,19 @@
+ 
 function rowSumOddNumbers(n) {
 if(n === 1){
   return 1
 } 
-  if(n === 2){
+ else if(n === 2){
     return 3+5
     }
-  if(n === 3){
+ else if(n === 3){
    return 7+9+11
   }
-  if(n === 4){
+  else if(n === 4){
     return 13+15+17+19
   }
-  if(n === 5){
+ else if(n === 5){
     return 21+23+25+27+29
   }
+  return 0
 }
