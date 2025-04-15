8383c90932486b7203c24fb1474faa2af5ba0291
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-04-15 19:14:34 +0200

    Update solution - CodeHub

--- 7-kyu/recursive-replication/recursive-replication.js
+++ 7-kyu/recursive-replication/recursive-replication.js
@@ -1,7 +1,7 @@
- 
 function replicate(times, number) {
 //  let arr = [number]
-if(times > 0){
-  return [number].concat(replicate(times-1, number))
+if(times <= 0){
+  return []
 }
+  return [number].concat(replicate(times-1,number))
 }
