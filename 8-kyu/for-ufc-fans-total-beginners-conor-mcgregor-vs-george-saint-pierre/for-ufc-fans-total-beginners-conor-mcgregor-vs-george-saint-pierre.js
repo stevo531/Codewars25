d1d72dfe078fe75abb5f8330c0e249f2cdd619e7
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-04-15 18:41:33 +0200

    Add solution - CodeHub

---
+++ 8-kyu/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre.js
@@ -0,0 +1,4 @@
+function quote(fighter) {
+  let fighterCase = fighter.toLowerCase()
+ return fighterCase === 'george saint pierre' ? "I am not impressed by your performance." :  "I'd like to take this chance to apologize.. To absolutely NOBODY!"  // your code here
+};
