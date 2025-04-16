feda71ca86fc438a50089c299ba7f38db2b0f4b3
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-04-16 11:22:30 +0200

    Update solution - CodeHub

--- 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.js
+++ 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.js
@@ -1,11 +1,10 @@
+ 
 function alphabetPosition(text) {
-  let splitText = text.replaceAll(' ', '').split('')
-  let alpha = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
- for(let i =1; i<alpha.length; i++){
-   let index = alpha[i].indexOf()
-   for(let i =1; i<splitText.length; i++){
-     let index2 = splitText[i].indexOf()
-   }
-   return splitText.map(index2 => index).join('')
+  let splitText = text.replaceAll(' ', '').toLowerCase().split('')
+  let alpha = 'abcdefghijklmnopqrstuvwxyz';
+ let mapped = splittext.map(char => {
+   let indexed = alpha.indexOf(char)
+   return indexed != -1 ? indexed + 1 : '';
+ })
  }
 }
