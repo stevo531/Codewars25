7a375bbd34306aa41b7b0cee8201f13ab83b02c5
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-04-16 11:05:50 +0200

    Update solution - CodeHub

--- 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.js
+++ 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.js
@@ -2,9 +2,9 @@ function alphabetPosition(text) {
   let splitText = text.replaceAll(' ', '').split('')
   let alpha = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
  for(let i =1; i<alpha.length; i++){
-   let index = indexOf(alpha[i])
+   let index = alpha[i].indexOf()
    for(let i =1; i<splitText.length; i++){
-     let index2 = indexOf(splitText[i])
+     let index2 = splitText[i].indexOf()
    }
    return splitText.map(index2 => index).join('')
  }
