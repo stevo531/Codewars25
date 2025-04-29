ada89a85501e5aa1b1cd46d86386c158226f52eb
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-04-29 09:51:06 +0200

    Update solution - CodeHub

--- 6-kyu/find-the-parity-outlier/find-the-parity-outlier.js
+++ 6-kyu/find-the-parity-outlier/find-the-parity-outlier.js
@@ -1,15 +1,15 @@
 function findOutlier(integers){
- let passEven = ''
- let passOdd = ''
+ let passEven = []
+ let passOdd = []
   //your code here
   for(let i=0; i<integers.length; i++){
     let num = integers[i];
     if(num % 2 === 0){
-      passEven += num;
+      passEven.push(num);
     }
     else {
-      passOdd += num;
+      passOdd.push(num);
     }
   }
-  return passEven.length === 1 ? passEven.length : passOdd.length
+  return passEven.length === 1 ? passEven : passOdd
 }
