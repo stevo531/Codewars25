32a8b73b026a428e071fa4475e8b04b115f182b3
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-04-29 10:26:48 +0200

    Update solution - CodeHub

--- 7-kyu/two-to-one/two-to-one.js
+++ 7-kyu/two-to-one/two-to-one.js
@@ -1,7 +1,6 @@
 function longest(s1, s2) {
 let combined = [...s1, ...s2];
   let unique= new Set(combined)
-  let final = unique.sort((a,b) => a-b)
-  return final
+  return unique
   // your code
 }
