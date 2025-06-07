506127917c8b0ef32ffb599d5909db0b6caee76b
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-06-07 16:28:30 +0200

    Update solution - CodeHub

--- 8-kyu/reversed-strings/reversed-strings.js
+++ 8-kyu/reversed-strings/reversed-strings.js
@@ -1,5 +1,5 @@
 function solution(str){
-  let reversedString = str.split('').reverse().join(' ');
+  let reversedString = str.split('e').reverse().join(' ');
   return reversedString;
 }
 console.log(solution('Hello world12'))
