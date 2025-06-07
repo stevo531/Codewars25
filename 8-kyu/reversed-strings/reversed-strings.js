7767efc663b4f0679cdab3e2b30634f4b2708960
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-06-07 16:29:34 +0200

    Update solution - CodeHub

--- 8-kyu/reversed-strings/reversed-strings.js
+++ 8-kyu/reversed-strings/reversed-strings.js
@@ -1,6 +1,6 @@
  
 function solution(str){
-  let reversedString = str.split('e').reverse().join('');
+  let reversedString = str.split('w').reverse().join('');
   return reversedString;
 }
 console.log(solution('Hello world12'))
