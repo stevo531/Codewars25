9a27c9991b41f704d57ef39f95d350a661c69ac2
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-06-07 16:20:49 +0200

    Update solution - CodeHub

--- 8-kyu/convert-a-number-to-a-string/convert-a-number-to-a-string.js
+++ 8-kyu/convert-a-number-to-a-string/convert-a-number-to-a-string.js
@@ -1,6 +1,8 @@
+ 
 function numberToString(num) {
  var numString = num.toString();
   return numString;
- // Return a string of the number here!
+ console.log(typeof(numString))
+  // Return a string of the number here!
 }
-console.log(typeof(numString))
+//console.log(typeof(numString))
