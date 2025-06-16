f4ff8cfa944ef2edcd66d2ff10f871e54742cfa8
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-06-16 11:53:33 +0200

    Update solution - CodeHub

--- 5-kyu/rgb-to-hex-conversion/rgb-to-hex-conversion.js
+++ 5-kyu/rgb-to-hex-conversion/rgb-to-hex-conversion.js
@@ -1,10 +1,6 @@
 const rgb = (r, g, b) => {
 ​
-  let hex = ''
-  hex += hexConversion(r)
-  hex += hexConversion(g)
-  hex += hexConversion(b)
-  return hex
+ 
 const valueRange = value => {
   value < 0 ? 0 : 0
   value > 255 ? 255: 255
@@ -19,5 +15,9 @@ if(hex.length !== 2){
   hex = '0'
   hex += value.toString(16)}
 }
-​
+ let hex = ''
+  hex += hexConversion(r)
+  hex += hexConversion(g)
+  hex += hexConversion(b)
+  return hex
 }
