f32879f29fadc14de689fd15ab69c7a67753f1a8
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-07-30 09:48:07 +0200

    Update solution - CodeHub

--- 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.js
+++ 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.js
@@ -4,7 +4,7 @@ let name = ''
 for(let i=0; i<splitted.length; i++){
   if(splitted[i] === 'www'){
     name+= splitted[i+1]
-  }
+  } else if(splitted[i] === 'com') name+=splitted[i-1]
 }
   return name
 }
