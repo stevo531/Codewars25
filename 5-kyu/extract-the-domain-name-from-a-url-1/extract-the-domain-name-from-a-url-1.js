c0f3027424b0381467ac393cd7f62e0c44fc1e1d
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-07-30 09:41:26 +0200

    Update solution - CodeHub

--- 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.js
+++ 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.js
@@ -2,8 +2,8 @@ function domainName(url) {
 let splitted = url.split('.')
 let name = ''
 for(let i=0; i<splitted.length; i++){
-  if(splitted[i] === '.'){
-    name+= splitted[i-1]
+  if(splitted[i] === 'www' && splitted[i+1] === '.'){
+    name+= splitted[i+2]
   }
 }
   return name
