5eed362c96caab64d38d0b2b9e883d6efc7545b3
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-07-30 11:50:31 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -1,11 +1,11 @@
 function pigIt(str){
-  let string = 
+  let string = ''
  for(let i=0; i<str.length; i++){
    string+=str[i]
    if(str[i] === ' '){
      continue
      string= string.split('').reverse(string.charAt(0).join(''))
    }
- 
+ }
   return string
 }
