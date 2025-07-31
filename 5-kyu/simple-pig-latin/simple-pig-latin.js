fff6c2e1426317a2d5fd39e5f79dd5e734d0d8d8
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-07-31 10:25:08 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -2,14 +2,11 @@ function pigIt(str){
 str=str.split(' ')
   let string=''
   for(let i=0; i<str.length;i++){
+    if(str[i].length === 1) string+=str[i]
     string+=str[i].slice(1) + str[i].charAt(0) + 'ay' + ' '  
   }
-  
- return string 
-  //   for(let i=0; i<str.length; i++){
-// str = 
-//     }
-//   return str
+  return string 
+​
 }
 console.log(pigIt('Pig latin is cool'))
 console.log(pigIt('This is my string'))
