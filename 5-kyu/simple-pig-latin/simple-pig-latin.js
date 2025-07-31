8d9f0ae0653b2db60a7e8a7246cb1beaade73521
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-07-31 10:07:53 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -1,5 +1,13 @@
- 
 function pigIt(str){
-str = str.slice(1) + str.charAt(0)
-  return str
+str=str.split(' ')
+  let string=''
+  for(let i=0; i<str.length;i++){
+    string+=str[i]
+  }
+  
+ return string 
+  //   for(let i=0; i<str.length; i++){
+// str = str.slice(1) + str.charAt(0)
+//     }
+//   return str
 }
