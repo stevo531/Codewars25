45c5eba5c4fbd1f333b6fc1aee1d9c2b3dd93db9
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-07-31 10:09:12 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -2,7 +2,7 @@ function pigIt(str){
 str=str.split(' ')
   let string=''
   for(let i=0; i<str.length;i++){
-    string+=str.slice(1) + str[0]
+    string+=str[i].slice(1) + str[0]
     break
   }
   
