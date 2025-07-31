f601cc027711f203c3fbb6cc73dcab104f5f5260
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-07-31 10:26:04 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -2,7 +2,7 @@ function pigIt(str){
 str=str.split(' ')
   let string=''
   for(let i=0; i<str.length;i++){
-    if(str[i].length === 1) string+=str[i]
+    if(str[i].length === 1) string+=`$ {str[i]}`
     string+=str[i].slice(1) + str[i].charAt(0) + 'ay' + ' '  
   }
   return string 
