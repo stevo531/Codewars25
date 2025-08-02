a811675eb74db2b709651610c84447e81dafbd12
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-08-02 10:20:42 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -4,7 +4,7 @@ str=str.split(' ')
   for(let i=0; i<str.length;i++){
    str[i].length === 1 
    ? string+=`$ {str[i]}`
-   : string+=str[i].slice(1) + str[i].charAt(0) + 'ay' + ' '  
+   : string+=str[i].slice(1) + str[i].charAt(0) + 'ay'   
     const regex = /[.,!?]/g
     const match = string.match(regex)
     if(str[i] === match) string+=str[i]
