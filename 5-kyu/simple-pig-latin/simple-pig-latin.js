49536a6f1a910861a7811dc93edf6887b71c348a
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-08-02 10:24:18 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -3,14 +3,14 @@ str=str.split(' ')
   let string=''
   for(let i=0; i<str.length;i++){
    str[i].length === 1 
-   ? string+=`$ {str[i]}`
+   ? string+=`${str[i]}`
    : string+=str[i].slice(1) + str[i].charAt(0) + 'ay' + ' '  
     const regex = /[.,!?]/g
     const match = string.match(regex)
     if(str[i] === match) string+=str[i]
     //if(regex.includes(str[i])) str+=str[i]
     }
-  return string.trim()
+  return string
 ​
 }
 console.log(pigIt('Pig latin is cool'))
