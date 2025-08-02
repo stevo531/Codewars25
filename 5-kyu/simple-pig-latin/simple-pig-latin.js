028e8e4317171c76ad07739c89aa96531aa5f01b
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-08-02 10:32:52 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -1,14 +1,12 @@
 function pigIt(str){
 str=str.split(' ')
   let string=''
+  let marks = ['.','!','?',',']
   for(let i=0; i<str.length;i++){
    str[i].length === 1 
    ? string+=`${str[i]}`
    : string+=str[i].slice(1) + str[i].charAt(0) + 'ay' + ' '  
-    const regex = /[.,!?]/g
-    const match = string.match(regex)
-    if(str[i] === match) string+=str[i]
-    //if(regex.includes(str[i])) str+=str[i]
+  if(marks.includes(str[i])) string+=str[i]
     }
   return string
 ​
