1c037af161c42b638738b034a39758473aaf24fb
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-08-05 09:37:40 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -1,11 +1,9 @@
- 
 function pigIt(str){
 str=str.split(' ')
   let marks = ['.','!','?',',']
  let final = str.map(word =>
-  return marks.includes(word)
+   marks.includes(word)
   ? word
   : word.slice(1) + word[0] + 'ay')
 }
-console.log(pigIt('Pig latin is cool'))
-console.log(pigIt('This is my string'))
+​
