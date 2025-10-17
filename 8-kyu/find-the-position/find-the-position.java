136b39fc1763701cb88107e36de44461d51a1dd7
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-17 17:51:42 +0200

    Update solution - CodeHub

--- 8-kyu/find-the-position/find-the-position.java
+++ 8-kyu/find-the-position/find-the-position.java
@@ -3,8 +3,8 @@ public class Kata
 {
   public static String position(char alphabet)
   {
-    String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-    System.out.println(alphabet.indexOf("a"));
-    //return "";
+    char[] letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    System.out.println(letters.indexOf("a"));
+ 
   }
 }
