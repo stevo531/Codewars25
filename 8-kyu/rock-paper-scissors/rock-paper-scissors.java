fcb1494777484f0f38afe0042df944adee4a4833
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-17 17:37:20 +0200

    Add solution - CodeHub

---
+++ 8-kyu/rock-paper-scissors/rock-paper-scissors.java
@@ -0,0 +1,21 @@
+public class Kata {
+  public static String rps(String p1, String p2) {
+    if(p1.equals("scissors") && p2.equals("paper")
+      || p1.equals("rock") && p2.equals("scissors")
+      || p1.equals("paper") && p2.equals("rock")){
+      return "Player 1 won!";
+    }
+    else if(p1.equals("scissors") && p2.equals("rock")
+           || p1.equals("rock") && p2.equals("paper")
+           || p1.equals("paper") && p2.equals("scissors")){
+      return "Player 2 won!";
+    }
+  
+  else if(p1.equals(p2)) {
+    return "Draw!";
+  }
+    else {
+      return "Invalid input!";
+    }
+}
+}
