3c008349789ba7f29f537ca83ef3d6d31b0adbec
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-17 17:45:11 +0200

    Add solution - CodeHub

---
+++ 8-kyu/find-the-position/find-the-position.java
@@ -0,0 +1,10 @@
+ 
+public class Kata
+{
+  public static String position(char alphabet)
+  {
+    char[] Alphabet = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+    System.out.println(Alphabet.indexOf("a"));
+    //return "";
+  }
+}
