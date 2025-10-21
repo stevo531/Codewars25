d84028c04fe368087e291f2530768ddb5523c17a
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-21 17:59:18 +0200

    Add solution - CodeHub

---
+++ 8-kyu/array-plus-array/array-plus-array.java
@@ -0,0 +1,13 @@
+public class Sum {
+ int total = 0;
+  public static int arrayPlusArray(int[] arr1, int[] arr2) {
+for(int i=0; i<arr1.length; i++){
+  total +=arr1[i];
+}
+    for(int i=0; i<arr2.length; i++){
+      total +=arr2[i];
+    }
+    return total;
+  }
+​
+}
