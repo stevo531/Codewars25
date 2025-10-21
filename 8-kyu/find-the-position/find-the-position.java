162fb6aa1a83be2be4c0db68bdf22d1ff1de989a
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-21 18:32:26 +0200

    Update solution - CodeHub

--- 8-kyu/find-the-position/find-the-position.java
+++ 8-kyu/find-the-position/find-the-position.java
@@ -3,10 +3,9 @@ public class Kata
 {
   public static String position(char alphabet)
   {
-    
-   // String[] letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+   
     ArrayList<String> finalLetters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
- for(int i=0; i<finalLetters.length; i++){
+ for(int i=0; i<finalLetters.size(); i++){
    if(finalLetters.get(alphabet)){
      return "Position of alphabet: "+ finalLetters.get(alphabet);
    }
