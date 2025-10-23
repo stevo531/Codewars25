97daaaa2774ff35826375d49437f90dad15071e3
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-23 16:58:45 +0200

    Add solution - CodeHub

---
+++ 8-kyu/sort-my-textbooks/sort-my-textbooks.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+​
+class sorter {
+​
+  public static List<String> sort(List<String> textbooks) {
+   Collections.sort(textbooks);
+    return textbooks;
+  }
+}
