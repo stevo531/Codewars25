a6cb306f87727b91af2ad5946a5f2d6419ffaebe
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-27 17:58:05 +0200

    Add solution - CodeHub

---
+++ 7-kyu/string-ends-with/string-ends-with.java
@@ -0,0 +1,11 @@
+ 
+public class Kata {
+  public static boolean solution(String str, String ending) {
+    if(str.split(ending)){
+    return true;
+  }
+    else {
+      return false;
+    }
+    }
+}
