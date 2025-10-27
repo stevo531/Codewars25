647a68d8c0d8e715d75276630e75c809334c86cc
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-27 18:00:42 +0200

    Update solution - CodeHub

--- 7-kyu/string-ends-with/string-ends-with.java
+++ 7-kyu/string-ends-with/string-ends-with.java
@@ -1,11 +1,7 @@
+ 
 public class Kata {
   public static boolean solution(String str, String ending) {
-    String[] splitString = str.split(ending);
-    if(splitString){
-    return true;
-  }
-    else {
-      return false;
-    }
+if(str.endsWith(ending)) return true;
+    return false;
     }
 }
