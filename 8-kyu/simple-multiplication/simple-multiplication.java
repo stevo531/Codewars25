ed29b2aa3e8f13eedb0a174e800535bbe464eceb
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-29 18:36:53 +0200

    Add solution - CodeHub

---
+++ 8-kyu/simple-multiplication/simple-multiplication.java
@@ -0,0 +1,9 @@
+public class Sid {
+    public static int simpleMultiplication(int n) {
+        //your code here
+      int num;
+      if(n % 2 == 0) num = n*8;
+       num = n*9;
+      return num;
+    }
+}
