dbd541f7b09576ffb0e903279ffbdd1bc29b8a69
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2025-10-30 16:31:48 +0200

    Add solution - CodeHub

---
+++ 6-kyu/find-the-odd-int/find-the-odd-int.java
@@ -0,0 +1,16 @@
+public class FindOdd {
+    public static int findIt(int[] a) {
+      int count =0;
+        for(int i=0; i<a.length; i++){
+          count = 0;
+          for(int j=0; j<a.length; j++){
+            if(a[i] == a[j]){
+              count++;
+            }
+          }
+          if(count % 2 != 0) return a[i];
+          
+        }
+      return -1;
+    }
+}
