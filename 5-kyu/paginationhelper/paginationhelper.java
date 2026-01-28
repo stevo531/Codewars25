e579d482f22e3193f0557e1fe04236cab7008052
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2026-01-28 12:12:48 +0200

    Update solution - CodeHub

--- 5-kyu/paginationhelper/paginationhelper.java
+++ 5-kyu/paginationhelper/paginationhelper.java
@@ -4,31 +4,32 @@ import java.util.List;
 ​
 import java.util.Collections;
 public class PaginationHelper<I> {
-  private List<I> collection;
-  private int itemsPerPage;
- 
-  public PaginationHelper(List<I> collection, int itemsPerPage) {
-  this.collection = collection;
-    this.itemsPerPage = itemsPerPage;                        
-  }
-  
-  public int itemCount() {
-    return collection.size();
-  }
- 
-  public int pageCount() {
-    return(int) Math.ceil((double) itemCount() / itemsPerPage);
-  }
+private List<I> collection;
+private int itemsPerPage;
 ​
-  public int pageItemCount(int pageIndex) {
-    if(pageIndex < 0 ||pageIndex>=pageCount()) return -1;
-    int startIndex = pageIndex * itemsPerPage;
-    int endIndex= Math.min(startIndex + itemsPerPage, itemCount());
-    return endIndex - startIndex;
-      }
-  
-  public int pageIndex(int itemIndex) {
-    if(itemIndex < 0 || itemIndex >= itemCount()) return -1;
-    return itemIndex / itemsPerPage;
-  }
+public PaginationHelper(List<I> collection, int itemsPerPage) {
+this.collection = collection;
+this.itemsPerPage = itemsPerPage;
+​
+}
+​
+public int itemCount() {
+return collection.size();
+}
+​
+public int pageCount() {
+return(int) Math.ceil((double) itemCount() / itemsPerPage);
+}
+​
+public int pageItemCount(int pageIndex) {
+if(pageIndex < 0 ||pageIndex>=pageCount()) return -1;
+int startIndex = pageIndex * itemsPerPage;
+int endIndex= Math.min(startIndex + itemsPerPage, itemCount());
+return endIndex - startIndex;
+}
+​
+public int pageIndex(int itemIndex) {
+if(itemIndex < 0 || itemIndex >= itemCount()) return -1;
+return itemIndex / itemsPerPage;
+}
 }
